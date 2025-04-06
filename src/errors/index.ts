525f57e2470d1ddbdfbfa04db356bb772cea9d53
royfreund <royfreund14@gmail.com>
2025-04-07 01:42:41 +0300

    Added all the custom errors

---
+++ src/errors/index.ts
@@ -0,0 +1,5 @@
+export * from './bad-request.error';
+export * from './base.error';
+export * from './internal-server.error';
+export * from './not-found.error';
+export * from './unauthorized.error';
